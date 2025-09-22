e01e5d130d2e57e55e1c474a3908b3f4105f0fe4
Caleb <caleb090@outlook.com>
2025-09-22 16:05:13 +0300

    Update index.php
    path added on cors

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ set_exception_handler(function ($e) {
 // Allow CORS
 // CORS headers - allow everything
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: *");
 
 // Handle preflight OPTIONS request
